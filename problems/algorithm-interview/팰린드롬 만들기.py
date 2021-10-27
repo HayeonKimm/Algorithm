7fdb224b4965a342f65520401328f22c48c4a76b
hayeonkim <hayeon.data@gmail.com>
2021-10-27 18:47:49 +0900

    Add 팰린드롬 만들기.py

---
+++ problems/algorithm-interview/팰린드롬 만들기.py
@@ -0,0 +1,24 @@
+# 팰린드롬 문제
+# 글자를 거꾸로 해도 뜻이 되게 만들기.
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        str=[]
+        
+        for char in s:
+            if char.isalnum():  # isalnum은 글자와 숫자만 허용하는 함수.
+                
+                strs.append(char.lower())
+                
+        
+        while len(Strs) > 1:
+            if strs.pop[0] != strs.pop()
+            
+            return False
+        
+        
+        return True
+        
